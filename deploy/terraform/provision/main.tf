906d41703bd17c10a726acf8368a0f3a1e3f2f89
Invvard <7305493+Invvard@users.noreply.github.com>
2021-03-28 19:01:11 -0400

    Remove the last function

--- deploy/terraform/provision/main.tf
+++ deploy/terraform/provision/main.tf
@@ -21,18 +21,3 @@ module "azfunc_dowepush" {
     environment = var.environment_name
   })
 }
-
-module "azfunc_maybe" {
-  source = "./modules/api"
-
-  project_name     = local.project_name
-  environment_name = var.environment_name
-  resource_prefix  = local.resource_prefix
-  rg_name          = azurerm_resource_group.api_resource_group.name
-  api_name         = "maybe"
-
-  location = var.location
-  tags     = merge(local.tags, {
-    environment = var.environment_name
-  })
-}
