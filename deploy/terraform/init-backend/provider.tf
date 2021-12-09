774a3e035ac301399206704e6f3da3d580244ca6
Invvard <7305493+Invvard@users.noreply.github.com>
2021-12-09 15:33:16 -0500

    Update TF et azurerm versions

--- deploy/terraform/init-backend/provider.tf
+++ deploy/terraform/init-backend/provider.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "~>0.14.0"
+  required_version = ">1.0.0"
   
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">2.48.0"
+      version = ">2.88.0"
     }
   }
 }
